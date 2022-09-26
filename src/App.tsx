fc70343fda7682177511fe386614c7a9ed2d0bef
Samille Machado <sami.bmachado@gmail.com>
2022-09-26 16:45:39 -0300

    installs e configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material/styles";
+import ThemeDefault from "./config/layout/Default";
 
-function App() {
+import AppRoutes from "./routes/AppRoutes";
+import StylesGlobal from "./config/theme/GlobalStyles";
+import { store } from "./store";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={ThemeDefault}>
+        <StylesGlobal />
+        <AppRoutes />
+      </ThemeProvider>
+    </Provider>
   );
-}
+};
 
 export default App;
